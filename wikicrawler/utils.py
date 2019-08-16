1abb4fed6a688b7b0229c3c3d3bf9a18629dc8aa
b-malone <bmalone.machine@gmail.com>
2019-08-16 15:52:46 -0700

    Experimenting with setting up topic models on data and crawling data and storing it for analysis and training.

---
+++ wikicrawler/utils.py
@@ -0,0 +1,22 @@
+import string
+from mltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+
+lemma = WordNetLemmatizer()
+punctuation = set(string.punctuation)
+stoplist = set(stopwords.words('english'))
+
+def remove_punctuation(text):
+    return ''.join(char for char in text if char not in punctuation)
+
+def remove_numbers(text):
+    return ''.join(char for char in text if char not in char.is_digit())
+
+def remove_stop_words(text):
+    return ' '.join([word for word in text.split() word not in stoplist])
+
+def remove_single_characters(text):
+    return ' '.join([word for word in text.split() if len(word) > 1])
+
+def lemmatize(text):
+    return ' '.join([lemma.lemmatize(word) for word in text.split()])
