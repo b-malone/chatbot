f82d1151df0493eb3404977521ff764880c1618a
b-malone <bmalone.machine@gmail.com>
2019-08-20 15:24:47 -0700

    LDA Model builds and counts topics! Also added pickling to save Corpus and LDA Model to save time re-running analysis on saved data and model.

--- wikipedia_topics/main.py
+++ wikipedia_topics/main.py
@@ -23,15 +23,15 @@ import utils
 
 # Configuration 
 NUM_PASSES=10
-NUM_TOPICS=100
+NUM_TOPICS=75
 RANDOM_STATE=1
 # ### LOGGING for Gensim
 logging.basicConfig()
 # ### Data Stores and backups
 DATABASE_FILE = 'data/wiki_content.db'
-LDA_BACKUP = '../data/lda_model'
-DICT_BACKUP = '../data/dictionary'
-CORPUS_BACKUP = '../data/corpus'
+LDA_BACKUP = 'data/lda_model'
+DICT_BACKUP = 'data/dictionary'
+CORPUS_BACKUP = 'data/corpus'
 
 # Setup and Model Building
 # #########################
@@ -56,64 +56,86 @@ lemma = WordNetLemmatizer()
 DATABASE=utils.get_file_path(DATABASE_FILE)
 content = content.Content(DATABASE)
 
-# EX: View a page
-# page_ids = content.get_page_ids()
-# page = content.get_page_by_id(page_ids[0])
-# print( str(page) )
-# print( type(content) )
-# print('=============')
+# Create a Dictionary
+## Vector Space of words and word_count
+dictionary = []
+try:
+    DICT=utils.get_file_path(DICT_BACKUP)
+    with open(DICT, "rb") as dict_file:
+        if dict_file:
+            print('Loading Dictionary File.')
+            dictionary = pickle.load(dict_file)
+            print('Dictionary Size = {}'.format(len(dictionary)))
+        else:
+            print('Building Dictionary...')
+            dictionary = corpora.Dictionary(content)    # list: (word_id, appearance count)
+            dictionary.filter_extremes(no_below=5, no_above=0.4)
+            print('Dictionary Size = {}'.format(len(dictionary)))
+except:
+    print('Building Dictionary...')
+    dictionary = corpora.Dictionary(content)    # list: (word_id, appearance count)
+    dictionary.filter_extremes(no_below=5, no_above=0.4)
+    print('Dictionary Size = {}'.format(len(dictionary)))
 
-# # Lemmatize and Clean Document
-# page = lemma.lemmatize(page)
-pages_content = content.get_cleaned_pages()
-print('=============')
-print('Pages of Content: {}'.format(len(pages_content)))
-# print('Which is OfType: {}'.format( type(pages_content) ))
-print('=============')
+# Create a Corpus
+corpus = []
+try:
+    CORPUS=utils.get_file_path(CORPUS_BACKUP)
+    with open(CORPUS, "rb") as corp_file:
+        if corp_file:
+            print('Loading Corpus File.')
+            corpus = pickle.load(corp_file)
+            print('Corpus Size = {}'.format(len(corpus)))
+        else:
+            print('Building Corpus...')
+            corpus = [dictionary.doc2bow(text) for text in content] # doc-to-bag_of_words
+            print('Corpus Size = {}'.format(len(corpus)))
+except:
+    print('Building Corpus...')
+    corpus = [dictionary.doc2bow(text) for text in content] # doc-to-bag_of_words
+    print('Corpus Size = {}'.format(len(corpus)))
 
-# for i in range(5):
-#     cursor = pages_content[i][0]
-#     obj = pages_content[i][1]
-#     # count = cursor.getColumnCount()
-#     # print( pages_content[i] )
-#     # print( json.dumps(cursor) )
+# Build LDA Model
+lda = []
+try:
+    LDA=utils.get_file_path(LDA_BACKUP)
+    with open(LDA, "rb") as lda_file:
+        if lda_file:
+            print('Loading LDA File.')
+            lda = pickle.load(lda_file)
+            print('Counting Topics...')
+            lda.print_topics(10) # Topic ~ set of (related) words
+        else:
+            print('Building LDA Model...')
+            lda = models.LdaModel(corpus, id2word=dictionary, random_state=RANDOM_STATE, num_topics=NUM_TOPICS, passes=NUM_PASSES)
+            print('Done!')
+            print('Counting Topics...')
+            topics = lda.print_topics(10) # Topic ~ set of (related) words
+            print(topics)
+except:
+    print('Building LDA Model...')
+    lda = models.LdaModel(corpus, id2word=dictionary, random_state=RANDOM_STATE, num_topics=NUM_TOPICS, passes=NUM_PASSES)
+    print('Done!')
+    print('Counting Topics...')
+    topics = lda.print_topics(10) # Topic ~ set of (related) words
+    print(topics)
 
-# # print( pages_content[0].fetchone() )
-# print('=============')
-
-#
-# !!!!!!!!!!
-# Dictionary being built wrong!
-#   Needs to be built on all content! NOT just (one) page.
-#   All pages' contents need to also be lammatized!
-#
-
-# dictionary = corpora.Dictionary(content)    # list: (word_id, appearance count)
-# dictionary = corpora.Dictionary(pages_content)    # list: (word_id, appearance count)
-# # dictionary.filter_extremes(no_below=5, no_above=0.4)
-# print(dictionary)
-# print(len(dictionary))
-# print('=============')
-
-# # Create a Corpus
-# corpus = [dictionary.doc2bow(text) for text in content] # doc-to-bag_of_words
-# print(len(corpus))
-
-# # Build LDA Model
-# lda = models.LdaModel(corpus, id2word=dictionary, random_state=RANDOM_STATE, num_topics=NUM_TOPICS, passes=NUM_PASSES)
-
-# # ??? Topics Count
-# lda.print_topics(lda) # Topic ~ set of (related) words
+# ??? Topics Count
+# print('Counting Topics...')
+# lda.print_topics(10) # Topic ~ set of (related) words
 
 
 
 # Save Model Structures
-# lda.save(LDA_BACKUP)
-# with open(DICT_BACKUP, "wb") as fp:
-#     pickle.dump(dictionary, fp)
-# fp.close()
-# with open(CORPUS_BACKUP, "wb") as fp:
-#     pickle.dump(corpora, fp)
+lda.save(LDA_BACKUP)
+with open(DICT_BACKUP, "wb") as fp:
+    pickle.dump(dictionary, fp)
+fp.close()
+with open(CORPUS_BACKUP, "wb") as fp:
+    pickle.dump(corpus, fp)
+fp.close()
+# with open(LDA_BACKUP, "wb") as fp:
+#     pickle.dump(lda, fp)
 # fp.close()
 
 # Run Queries!
